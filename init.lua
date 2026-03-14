caf0abcf51d2c49a4682b725796ccd50a3eec35b
Panic_mike <91552716+PanicMike-9@users.noreply.github.com>
2026-03-14 23:18:45 +0530

    Initialize Neovim configuration with key mappings

--- init.lua
+++ init.lua
@@ -1,1 +1,58 @@
+-- Bootstrap lazy.nvim
+vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
 
+-- set new leader
+vim.g.mapleader = " "
+
+-- lazy plugins setup
+require("lazy").setup("plugins")
+
+-- Basic settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+
+-- set custom color 
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#13141c", underline = false })
+
+-- show current mode
+vim.opt.showmode = false
+
+-- keymappings
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+
+-- keymappings for LSP
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+
+-- keymappings to jump between errors quickly
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+
+-- telescope(file explorer) keymappings
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files)
+vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+vim.keymap.set("n", "<leader>fb", builtin.buffers)
+vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+
+-- sidebar file explorer(nvim-tree) keymappings
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- buffer switching keymappings
+vim.keymap.set("n", "<S-l>", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<S-h>", ":BufferLineCyclePrev<CR>")
